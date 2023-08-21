9dc874175fd593d898d0217b19ad13d051dce505
Shashwat Saxena <shashwat.saxena12@gmail.com>
2023-08-21 14:56:16 +0530

    Module-4 Sol

--- Module-4 Solution/easier/SpeakHello.js
+++ Module-4 Solution/easier/SpeakHello.js
@@ -1,7 +1,7 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-(function (window) {
+(function (window){
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
@@ -13,7 +13,7 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
+ helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
 
